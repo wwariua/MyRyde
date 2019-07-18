9317fca97704b0b00d53e643937219f87837f298
wjwahome <wjwahome@gmail.com>
2019-07-18 13:07:53 +0300

    Initial commit

---
+++ app/src/main/java/com/doit/myryde/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.doit.myryde;
+
+public class PayPalConfig
+{
+    public static final String PAYPAL_CLIENT_ID = "AXCIZJ-6rLnwBMhHMkuaQSpdbKyBlbcwWilllfraeC07qEvdU12DiM_b2biBZzydhXski8KUkpF5SzCOI";
+}
